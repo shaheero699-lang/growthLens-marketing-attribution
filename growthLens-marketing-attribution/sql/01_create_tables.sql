2f7d5c2ab588cfb84b13557fa525bbe04d0b73b0
Shaheer Mohamed <shaheero699@gmail.com>
2026-06-11 18:03:41 +0530

    Update 01_create_tables.sql

--- growthLens-marketing-attribution/sql/01_create_tables.sql
+++ growthLens-marketing-attribution/sql/01_create_tables.sql
@@ -1,38 +1,20 @@
--- Check Total Records
-SELECT COUNT(*) AS total_records
-FROM marketing_raw;
+-- GrowthLens Marketing Attribution
+-- Create Raw Marketing Table
 
--- Check Null Values
-SELECT *
-FROM marketing_raw
-WHERE customer_id IS NULL
-   OR session_id IS NULL
-   OR channel IS NULL;
-
--- Check Duplicate Customer Sessions
-SELECT
-    customer_id,
-    session_id,
-    COUNT(*) AS duplicate_count
-FROM marketing_raw
-GROUP BY customer_id, session_id
-HAVING COUNT(*) > 1;
-
--- Check Negative Values
-SELECT *
-FROM marketing_raw
-WHERE impressions < 0
-   OR clicks < 0
-   OR conversions < 0
-   OR ad_spend < 0
-   OR revenue < 0;
-
--- Check Conversion Logic
-SELECT *
-FROM marketing_raw
-WHERE conversions > clicks;
-
--- Check Click Logic
-SELECT *
-FROM marketing_raw
-WHERE clicks > impressions;
+CREATE TABLE marketing_raw (
+    customer_id VARCHAR(50),
+    session_id VARCHAR(50),
+    activity_date DATE,
+    channel VARCHAR(50),
+    campaign_name VARCHAR(100),
+    impressions INT,
+    clicks INT,
+    ad_spend DECIMAL(10,2),
+    conversions INT,
+    revenue DECIMAL(10,2),
+    customer_ltv DECIMAL(10,2),
+    device_type VARCHAR(20),
+    region VARCHAR(50),
+    touchpoint_order INT,
+    journey_id VARCHAR(50)
+);
